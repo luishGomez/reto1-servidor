a6d8a9ea1e68af7f7e41a79a1a6569e244e72127
2dam <2dam@HZ341352.TartangaLH.eus>
2019-10-21 13:39:08 +0200

    Primer commit inicio de server e hilos.

--- src/main/SocketHilo.java
+++ src/main/SocketHilo.java
@@ -1,14 +1,74 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 package main;
 
+import clases.Mensaje;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
- *
- * @author 2dam
+ * Es la clase que tiene el hilo que maneja el lado servidor de una consulta de
+ * un cliente.
+ * Is the server side class with the thread to manage the consult from the user
+ * to the server.
+ * @author Ricardo Peinado Lastra
  */
-public class SocketHilo {
-    
+public class SocketHilo extends Thread{
+    private static final java.util.logging.Logger LOGGER = java.util.logging.Logger.getLogger("main.Servidor");
+    private Socket miSocket;
+    private boolean libre;
+    public SocketHilo(Socket miSocket,boolean libre){
+        this.miSocket = miSocket;
+        this.libre = libre;
+    }
+    public void run(){
+        ObjectInputStream flujo_entrada = null;
+        ObjectOutputStream flujo_salida = null;
+        try{
+            flujo_entrada=new ObjectInputStream(miSocket.getInputStream());
+            flujo_salida= new ObjectOutputStream(miSocket.getOutputStream());
+            
+            //Empezamos a interpretar
+            if(libre){
+                try{
+                Mensaje mensajeInicio = (Mensaje) flujo_entrada.readObject();
+                    switch(mensajeInicio.getOpc()){
+                        case 1:
+                            //if()
+                    }
+                }catch(ClassNotFoundException e){
+                    LOGGER.severe(e.getMessage());
+                }
+            }else{
+            
+            }
+            
+           
+            flujo_salida.close();
+            flujo_entrada.close();
+            miSocket.close();
+        }catch(IOException e){
+            LOGGER.severe(e.getMessage());
+        }finally{
+            try{
+                if(flujo_salida!=null)
+                       flujo_salida.close();
+                if(flujo_entrada!=null)
+                    flujo_entrada.close();
+                if(miSocket!=null)
+                    miSocket.close();
+            }catch(IOException e){
+                LOGGER.severe(e.getMessage());
+            }
+        }
+        
+        
+    }
 }
