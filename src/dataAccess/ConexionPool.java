2ebcac93be8a32924e5096a864d099cd6e72f8bc
2dam <2dam@MACINF02.TartangaLH.eus>
2019-10-21 12:45:49 +0200

    comit de pool y daoFactory

--- src/dataAccess/ConexionPool.java
+++ src/dataAccess/ConexionPool.java
@@ -5,10 +5,57 @@
  */
 package dataAccess;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Stack;
+
+//import java.util.Stack;
+
 /**
- *
- * @author 2dam
+ * Conexion con la base de datos
+ * @author sergio
  */
+import java.util.ResourceBundle;
+//import java.util.logging.Logger;
 public class ConexionPool {
+    private   static String config;
+    private   static ResourceBundle configFile;
+    protected static Stack pool;
+    protected static String connectionURL;
+    protected static String userName;
+    protected static String password;
+    
+    /**
+     *inicializa los valores de conexion a la base a partir del config
+     */
+    public ConexionPool(){
+        configFile=ResourceBundle.getBundle(config);
+        connectionURL = configFile.getString("Conn");
+        userName = configFile.getString("DBUser");
+        password = configFile.getString("DBPass");
+        pool = new Stack();
+    }
+    /**
+     * crea una conexin a la base de datos
+     * @return una conexion nueva o una ya abierta
+     * @throws SQLException 
+     */
+    public synchronized static Connection extraerConexion() throws SQLException{   
+	   if(!pool.empty()) { 
+               return (Connection) pool.pop();
+           } 
+           else {  
+               return DriverManager.getConnection(connectionURL, userName, password);
+	   } 
+    }
+    /**
+     * liberamos la conexion a la base de datos
+     * @param conn la conexion que se quiere cerrar
+     * @throws SQLException
+     */
+     public synchronized static void liberarConexion(Connection conn)throws SQLException{ 
+	    pool.push(conn);
+    }
     
 }
