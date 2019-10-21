a45c2ec3a73bf09587e5b43c20c67a99202c7348
2dam <2dam@HZ341352.TartangaLH.eus>
2019-10-21 13:39:08 +0200

    Primer commit inicio de server e hilos.

--- src/main/Servidor.java
+++ src/main/Servidor.java
@@ -5,10 +5,51 @@
  */
 package main;
 
+import clases.Mensaje;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
- *
- * @author 2dam
+ * La clase main del lado servidor.
+ * The main class of the server side.
+ * @author Ricardo Peinado Lastra
  */
 public class Servidor {
+    private static final java.util.logging.Logger LOGGER = java.util.logging.Logger.getLogger("main.Servidor");
+    private static int puerto = Integer.parseInt(ResourceBundle.getBundle("main.serverConf").getString("port"));
+    private static int hilos = Integer.parseInt(ResourceBundle.getBundle("main.serverConf").getString("threads"));
+    private static int contador=0;    
     
+    public static void main(String[] args){
+        ServerSocket serverSocket=null;
+        try{
+           serverSocket = new ServerSocket(puerto);
+           while(true){
+               //Version 1
+               Socket socket = serverSocket.accept();
+               if(contador<=hilos){
+               
+               }else{
+                   ObjectInputStream flujoEntrada = new ObjectInputStream(socket.getInputStream());
+                   ObjectOutputStream flujoSalida= new ObjectOutputStream(socket.getOutputStream());
+                   Object objeto = flujoEntrada.readObject();
+                   Mensaje mensaje = new Mensaje(-1,"Estamos ocupados");
+                   flujoSalida.writeObject(mensaje);
+                   flujoEntrada.close();
+                   flujoSalida.close();
+                   socket.close();
+               }
+           }
+        }catch(IOException e){
+            LOGGER.severe(e.getMessage());
+        } catch (ClassNotFoundException e) {
+            LOGGER.severe(e.getMessage());
+        }
+    }
 }
